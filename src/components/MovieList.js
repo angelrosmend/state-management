d752ac66ff89a472631ca1d1f9c84e5c4b41c7f2
angelrosmend <angelrosmend@gmail.com>
2020-05-12 21:16:49 -0300

    first commit

---
+++ src/components/MovieList.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const [movies, setMovies] = useState([
+ {
+  name: 'Inception',
+  price: '$10',
+  id: 1
+ },
+ {
+  name: 'Arrival',
+  price: '$11',
+  id: 2
+ },
+ {
+  name: 'Donnie Darko',
+  price: '$13',
+  id: 3
+ }
+]);
+
+ return (
+  <div>
+   {movies.map(movie => (
+       <Movie name={movie.name} price={movie.price} key={movie.id}/>
+   ))}
+  </div>
+ );
+
+export default MovieList
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Nav() {
+ return (
+  <div>
+   <h3>Dev Ed</h3>
+   <p>List of Movies: </p>   
+  </div>
+ )
+}
+
+export default Nav
