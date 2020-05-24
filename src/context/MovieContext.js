9307bd2f2a4117fd0d94bc5e076ef138e282d78a
Angel Rosmend <60056902+angelrosmend@users.noreply.github.com>
2020-05-24 15:56:55 -0300

    Update MovieContext.js

--- src/context/MovieContext.js
+++ src/context/MovieContext.js
@@ -1,7 +1,6 @@
 import React, { useState, createContext} from 'react'
 
 export const MovieContext = createContext();
-
 export const MovieProvider = (props) => {
  const [movies, setMovies] = useState([
   {
